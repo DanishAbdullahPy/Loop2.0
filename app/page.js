f680b606bddff6baf2042bb3364185d0405158a8
Danish <danishabdullah276@gmail.com>
2024-09-01 00:36:17 +0530

    first commit

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div>
+    <h1>hi there </h1>
+    <Button> hi there click me</Button>
+    
+   </div>
+  );
+}
