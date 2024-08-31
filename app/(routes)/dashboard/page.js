4d875df9c5885f0c82bb2ba91ebf1b3ef3148207
Danish <danishabdullah276@gmail.com>
2024-09-01 00:45:32 +0530

    auth done ,userprofile done

--- app/(routes)/dashboard/page.js
+++ app/(routes)/dashboard/page.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Header } from './_components/Header'
+import { UserButton } from '@clerk/nextjs'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div>
+<Header/>
+<UserButton/>
+    </div>
   )
 }
 
